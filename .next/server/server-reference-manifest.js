a2c89f7ad269f3a6d885aec2b4e769ce193ea4e4
gss-patricia <gss.patricia@gmail.com>
2024-02-22 21:43:25 +0000

    remove e ignora arquivos .next

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"+LT3Wk0f6PpPx/wLSOXQsn5qCVDEllXBfcA1C5rZ4Bk=\"\n}"
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,22 @@
 import styles from "./page.module.css";
 import { Categorias } from "./components/Categorias";
 import { Produtos } from "./components/Produtos";
-import { getCategorias, getTodosProdutos } from "../lib/api";
+import { getCategorias } from "../lib/api";
+
+async function fetchProdutosApi() {
+  const res = await fetch("http://localhost:3000/api/produtos");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const produtos = await res.json();
+
+  return produtos;
+}
 
 export default async function Home() {
-  const produtos = getTodosProdutos();
+  const { produtos } = await fetchProdutosApi();
   const categorias = getCategorias();
 
   return (
